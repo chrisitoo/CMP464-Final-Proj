88a514ee879a4b9f90bb78d1df1d94d1165095d6
Chris <Chrisito1127@gmail.com>
2023-12-22 21:50:50 -0500

    Add input

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
+import React, { useState } from "react";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let toDos = ["Errand 1 ", "Errand 2", "Errand 3", "Errand 4", "Errand 5"];
+  const [toDos, setToDos] = useState<string[]>([
+    "Errand 1",
+    "Errand 2",
+    "Errand 3",
+    "Errand 4",
+    "Errand 5",
+  ]);
 
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
+
+  const handleAddItem = (newItem: string) => {
+    setToDos((prevToDos) => [...prevToDos, newItem]);
+  };
+
   return (
     <div>
       <ListGroup
         items={toDos}
         heading="Errands"
         onSelectItem={handleSelectItem}
+        onAddItem={handleAddItem}
       />
     </div>
   );
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,42 @@
+import { ChangeEvent, MouseEvent, useState } from "react";
+
 interface Props {
   items: string[];
   heading: string;
-
   onSelectItem: (item: string) => void;
+  onAddItem: (newItem: string) => void;
 }
 
-import { MouseEvent, useState } from "react";
-
-function ListGroup({ items, heading, onSelectItem }: Props) {
+function ListGroup({ items, heading, onSelectItem, onAddItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleInputKeyPress = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.key === "Enter" && inputValue.trim() !== "") {
+      const newItems = [...items, inputValue.trim()];
+      setSelectedIndex(newItems.length - 1);
+      onSelectItem(newItems[newItems.length - 1]);
+      setInputValue("");
+      onAddItem(newItems[newItems.length - 1]);
+    }
+  };
 
   return (
     <>
       <h1>{heading}</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyDown={handleInputKeyPress}
+        placeholder="Add new item..."
+      />
       {items.length === 0 && <p> Add something to keep track of your day!</p>}
       <ul className="list-group">
         {items.map((item, index) => (
